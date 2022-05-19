77a205754e8ff3cebd0486c3741f5476a6882e61
Qesis-Yonas <yonasweldetsion12@gmail.com>
2022-05-19 16:12:34 -0500

    commited

--- Astonomy-Picture-of-the-Day-Lab/server.js
+++ Astonomy-Picture-of-the-Day-Lab/server.js
@@ -1,29 +1,60 @@
 $(document).ready(
     function () {
-        $("#view_button").click(getPicture);
+    $("#view_button").click(getPicture);
     });
-//connection
-function getPicture() {
+    function getPicture () {
     $.ajax({
-        url: "https://api.nasa.gov/planetary/apod",
-        // ?api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch?
-        type: "GET",
-        data: {
-            api_key: "DEMO_KEY",
-            date: $("date").val()
-        },
-        dataType: "json",
-        "success": displayPicture,
-        "error": failedDisplay
+    url: "https://api.nasa.gov/planetary/apod",
+    type: "GET",
+    data: { 
+        //api_key: "cNv5U4640THADS9vvLTdaBZ3Jnx43q19Q1JXGbMu",
+        api_key: "DEMO_KEY",
+    date: $("date").val() },
+    dataType: "json",
+    "success": showPicture,
+    "error": noPicture
     });
-};
-//displaying picture from the API
-function displayPicture(data) {
-
+    };
+    function showPicture(data) {
     $("#pic").attr("src", data.url);
     $("#title").text(data.title);
-};
-//error handler
-function failedDisplay(error) {
+    };
+    function noPicture(error) {
     alert(error.responseText);
-};
+    };
+
+
+
+
+
+
+
+// $(document).ready(
+//     function () {
+//         $("#view_button").click(getPicture);
+//     });
+// //connection
+// function getPicture() {
+//     $.ajax({
+//         url: "https://api.nasa.gov/planetary/apod",
+//         // ?api_key=Bdzl1U2O1v2kvtYRWGGeohUaulxpN6QwbiEwi3Ch?
+//         type: "GET",
+//         data: {
+//             api_key: "DEMO_KEY",
+//             date: $("date").val()
+//         },
+//         dataType: "json",
+//         "success": displayPicture,
+//         "error": failedDisplay
+//     });
+// };
+// //displaying picture from the API
+// function displayPicture(data) {
+
+//     $("#pic").attr("src", data.url);
+//     $("#title").text(data.title);
+// };
+// //error handler
+// function failedDisplay(error) {
+//     alert(error.responseText);
+// };
